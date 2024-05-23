b8be60a54b93927f3ffcb38c79a5ef7db94161b6
Rujiravee (Vee) <rujiravee.kongdee@postgrad.manchester.ac.uk>
2024-05-23 18:14:53 +0100

    Set up fix to height

--- oodood-code/src/App.js
+++ oodood-code/src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App app-container">
       <Container className="px-4 px-lg-5 pt-5">
         <Row className="gx-4 gx-lg-5 align-items-center my-5">
-          <Col
-            lg={5}
-            className="d-flex justify-content-center align-items-center"
-          ></Col>
-          <Col lg={7}></Col>
+          <Card>
+            <Card.Body>
+              <Card.Title>Special title treatment</Card.Title>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional
+                content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
         </Row>
       </Container>
     </div>
